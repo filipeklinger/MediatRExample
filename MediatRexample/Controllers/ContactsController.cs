e472d3e95dc11e8e40328e32ae23ce022b349412
Filipe Klinger <filipe@tg4.com.br>
2020-11-21 14:05:11 -0300

    Iniciando Reppo

---
+++ MediatRexample/Controllers/ContactsController.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using MediatRexample.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static MediatRexample.Data.ContractContext;
+
+namespace MediatRexample.Controllers
+{
+    [Route("api/contacts")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ContactsController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet("{id}")]
+        public async Task<Contact> GetContact([FromRoute]Query query)
+        {
+            return await _mediator.Send(query);
+        }
+
+        #region Nested Classes
+        public class Query : IRequest<Contact>
+        {
+            public int Id { get; set; }
+        }
+
+        public class ContractHandler : IRequestHandler<Query, Contact>
+        {
+            private ContractContext db;
+
+            public ContractHandler(ContractContext db) => this.db = db;
+
+            public Task<Contact> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return this.db.Contacts.Where(c => c.Id == request.Id).SingleOrDefaultAsync();
+            }
+        }
+        #endregion
+    }
+}
---
+++ MediatRexample/Data/ContractContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MediatRexample.Data
+{
+    //Classe somente de Exemplo não é necessario para uso do MediatR
+    public class ContractContext : DbContext
+    {
+        public ContractContext(DbContextOptions<ContractContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            model.Entity<Contact>().HasData(
+                new Contact { Id= 1, FirstName = "Steve", LastName = "Micheloti"},
+                new Contact { Id = 2, FirstName = "Bill", LastName = "Gattes" },
+                new Contact { Id = 3, FirstName = "Satina", LastName = "NaDela" }
+                );
+        }
+
+        public class Contact
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
